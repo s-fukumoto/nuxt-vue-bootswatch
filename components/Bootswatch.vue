84ac3c4d2432628e134bb047c110aea5427dd7ef
S.Fukumoto <s-fuku@kuru.jp>
2017-10-04 02:55:37 +0900

    Select Component 実装

---
+++ components/Bootswatch.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <select v-model="selected_item">
+      <option v-for="theme in themes" :value="theme.name">
+        {{ theme.name }}
+      </option>
+    </select>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+const API_BOOTSWATCH = 'https://bootswatch.com/api/3.json'
+
+export default {
+  async created () {
+    let { data } = await axios.get(API_BOOTSWATCH)
+    return { themes: data.themes }
+  },
+  data () {
+    return {
+      selected_item: '',
+      themes: []
+    }
+  }
+}
+</script>
